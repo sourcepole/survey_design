858ccb919bf9faea58dd6fd1429215f4e7c7eb5d
Marco Hugentobler <marco.hugentobler@sourcepole.ch>
2016-10-13 14:10:44 +0200

    Remove survey evaluation widget

--- surveydesignplugin.py
+++ surveydesignplugin.py
@@ -4,7 +4,6 @@ from qgis.core import *
 from qgis.gui import *
 from surveyinitdialog import SurveyInitDialog
 from surveydesigndialog import SurveyDesignDialog
-from surveyevaluationdialog import SurveyEvaluationDialog
 from surveyevaluation import SurveyEvaluation
 
 class SurveyDesignPlugin:
@@ -21,11 +20,8 @@ class SurveyDesignPlugin:
         self.actionSurveyProperties = QAction( 'Survey design',  self.iface.mainWindow() )
         QObject.connect( self.actionSurveyProperties, SIGNAL("triggered()"), self.initSurveyDesign)
 
-        self.actionEvaluateSurvey = QAction( 'Survey evaluation',  self.iface.mainWindow() )
-        QObject.connect( self.actionEvaluateSurvey,  SIGNAL("triggered()"),  self.openSurveyEvaluationDialog )
         self.checkSurveyDesignPossible()
         surveyDesignMenu.addAction(  self.actionSurveyProperties )
-        surveyDesignMenu.addAction( self.actionEvaluateSurvey )
         self.surveyDesignAction = menuBar.addMenu( surveyDesignMenu )
 
         QObject.connect( self.iface, SIGNAL('projectRead()'), self.projectLoaded )
@@ -43,31 +39,9 @@ class SurveyDesignPlugin:
         self.designDialog = SurveyDesignDialog( self.iface.mainWindow(),  self.iface )
         self.designDialog.show()
 
-    def openSurveyEvaluationDialog(self):
-        dialog = SurveyEvaluationDialog( self.iface,  self.iface.mainWindow() )
-        if dialog.exec_() != QDialog.Accepted:
-            return
-
-        #check for which strata to run the analysis (the user has to change the 'ok' attribute of the baselines to something different than 'f')
-        strataExcludeSet = set()
-        baselineClipLayerId = dialog.baselineClipLayerId()
-        if not baselineClipLayerId.isEmpty():
-            baselineClipLayer = QgsMapLayerRegistry.instance().mapLayers()[ baselineClipLayerId ]
-            f = QgsFeature()
-            fIt = baselineClipLayer.getFeatures( QgsFeatureRequest() )
-            while fIt.nextFeature( f ):
-                okString = f.attribute( "ok" )
-                stratumId = f.attribute["stratum_id"].toInt()[0]
-                if okString == u"f" or okString == u"F":
-                    strataExcludeSet.add( stratumId )
-
-        eval = SurveyEvaluation( self.iface,  dialog.sampleLayerId(), dialog.stratumId(),  dialog.arealAvailability(),  dialog.catch(),  dialog.dist(),  dialog.width(),  dialog.verticalAvailability(),  strataExcludeSet )
-        eval.evaluateSurvey( dialog.speciesVulnerability() )
-
     def checkSurveyDesignPossible( self ):
             projectContainsDesign = self.projectContainsSurveyDesign()
             self.actionSurveyProperties.setEnabled( True )
-            self.actionEvaluateSurvey.setEnabled( True )
 
     def projectContainsSurveyDesign(self):
         surveyAreaLayerId = QgsProject.instance().readEntry( "Survey",  "SurveyAreaLayer" )[0]
